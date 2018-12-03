9998d5282637a42c091dcad88084363356d9e116
Ben Mangold <benmangold@gmail.com>
2018-12-02 21:48:45 -0500

    separating gallery styles

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const parser = require('body-parser')
-const path = require('path')
+const parser = require('body-parser');
+const path = require('path');
 const cors = require('cors');
-const db = require('../db/index.js')
+const db = require('../db/index.js');
 
 /* Initialize Express App */
 let app = express();
@@ -15,34 +15,32 @@ app.use(parser.json());
 /* Routes */
 
 app.get('/buy/:productName', (req, res) => {
-  res.sendFile(path.resolve('react-client/dist/index.html'))
-})
- 
-app.get('/productImages/:productName', (req,res) => {
-  let productName = req.url.split('/')[2]
+  res.sendFile(path.resolve('react-client/dist/index.html'));
+});
+
+app.get('/productImages/:productName', (req, res) => {
+  let productName = req.url.split('/')[2];
   db.getProductRecord(productName, (err, data) => {
-    if(err) {
-      if (err.message === '404') res.status(404).send(err)
-      else res.status(500).send(err)
+    if (err) {
+      if (err.message === '404') res.status(404).send(err);
+      else res.status(500).send(err);
+    } else {
+      res.send(data);
     }
-    else {
-      res.send(data)
-    }
-  })
-})
+  });
+});
 
 /* Use Test Data in testJson.js */
 app.post('/productImages', (req, res) => {
   db.createProductRecord(req.body, (err, record) => {
     if (err) {
-      console.log(err)
-      res.status(500).send(err)
-    }
-    else res.send(record)
+      console.log(err);
+      res.status(500).send(err);
+    } else res.send(record);
   });
-})
+});
 
 /* Start Server */
 app.listen(3000, () => {
-  console.log('listening on 3000')
-})
+  console.log('listening on 3000');
+});
